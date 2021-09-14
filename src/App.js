9ac820d44751e81869b14b598bf3b30978cab232
sentinelbot <ikmazameti@gmail.com>
2021-09-14 17:30:45 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import "./App.css";
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//components
+import Home from "./components/screens/home/Home";
+import Contact from "./components/screens/contact/Contact";
+import Map from "./components/screens/map/Map";
+import Navigation from "./components/NavBar/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 
 export default function App() {
   return (
-    <div>
-      <p>xup</p>
+    <div className="App">
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Contact",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Connectivity Map",
+    url: "#",
+    cName: "nav-links",
+  },
+];
