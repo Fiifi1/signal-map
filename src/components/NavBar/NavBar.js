70f672136651cb3e6c09c2a7c3309b5be20f0132
sentinelbot <ikmazameti@gmail.com>
2021-09-16 01:27:54 +0000

    navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,14 +3,23 @@ import { MenuItems } from "./MenuItems";
 import "./NavBar.css";
 
 export default class NavBar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
   render() {
     return (
       <nav className="NavBarItems">
         <h1 className="navbar-logo">
+          React
           <i className="fab fa-react"></i>
         </h1>
-        <div className="menu-icon"> </div>
-        <ul>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
           {MenuItems.map((item, index) => {
             return (
               <li key={index}>
